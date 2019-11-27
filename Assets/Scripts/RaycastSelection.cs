d0fd035a2047472e490a35e3e46bed73b351f3d7
FleetCommand <38727802+FleetCommander@users.noreply.github.com>
2019-11-27 18:09:50 +0100

    RaycastSelection debug

---
+++ Assets/Scripts/RaycastSelection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastSelection : MonoBehaviour{
+
+    float maxDistance = 500;
+
+
+    void Start(){
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Debug.Log(gameObject.transform.position);
+        Debug.DrawRay(gameObject.transform.position, transform.TransformDirection(Vector3.forward), Color.red, 0, true);
+        
+    }
+
+}
