e3eb5cc4133e1d2d2ab7ffe3033bcba5b5bf604a
FleetCommand <38727802+FleetCommander@users.noreply.github.com>
2019-11-27 15:43:29 +0100

    - Plattformtrigger funktioniert.
    - Bug fixes ( GrabVolume Material geändert )
    - Handschuhe transparenter

---
+++ Assets/Plattformtrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plattformtrigger : MonoBehaviour
+{
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.GetComponent<MeshRenderer>().material.color == other.gameObject.GetComponent<MeshRenderer>().material.color)
+        {
+            Debug.Log(gameObject.name + " was triggered by " + other.gameObject.name);
+        }
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        // other.gameObject.Getcomponent<MeshRenderer>().material.color()
+    }*/
+}
