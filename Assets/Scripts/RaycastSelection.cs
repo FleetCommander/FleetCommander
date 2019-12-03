df6c6a10758cb5bddc77b87cf389b1d0f6e90eef
FleetCommand <38727802+FleetCommander@users.noreply.github.com>
2019-12-03 14:09:15 +0100

    Raycast:
    Sichtbarer Raycast aus der rechten Hand

--- Assets/Scripts/RaycastSelection.cs
+++ Assets/Scripts/RaycastSelection.cs
@@ -4,27 +4,51 @@ using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
 public class RaycastSelection : MonoBehaviour {
+
     private const string SELECTABLE = "Selectable"; // TODO ali, Tags in ENUMS auslagern
-    private LineRenderer lineRenderer;
-    private Vector3[] points = new Vector3[2];
+
+    [SerializeField]
+    private GameObject dot;
+
+    private Material material;
+    
+    private float defaultLength = 5.0f;
+
+    private LineRenderer lineRenderer = null;
 
 
-    void Start() {
+    void Awake() {
         lineRenderer = GetComponent<LineRenderer>();
-        lineRenderer.positionCount = 2;
     }
 
     void Update() {
+        RenderRaycast();
+    }
+    
 
+    private void RenderRaycast() {
 
-        points[0] = transform.position;
-        points[1] = transform.forward * 1000;
+        float targetLength = defaultLength;
+        RaycastHit hit = CreateRaycast(targetLength);
+
+        Vector3 endPosition = transform.position + (transform.forward * targetLength);
+        dot.transform.position = endPosition;
+
         
-        lineRenderer.SetPositions(points);
-        Debug.DrawRay(transform.position, transform.forward * 1000, Color.red, 0);
+        if (hit.collider != null) {
+            endPosition = hit.point;
 
+        }
+        
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, endPosition);
+    }
 
-      
+    private RaycastHit CreateRaycast(float length) {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        Physics.Raycast(ray, out hit, defaultLength);
+        return hit;
     }
 
 }
