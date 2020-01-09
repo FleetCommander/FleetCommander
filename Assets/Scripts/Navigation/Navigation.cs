2a955a1ec60e2644f20b5c6ef3d31f3b47e4cab3
FleetCommand <38727802+FleetCommander@users.noreply.github.com>
2020-01-09 15:03:31 +0100

    Vorbereitungen zur Navigation

---
+++ Assets/Scripts/Navigation/Navigation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigation : MonoBehaviour {
+    private Vector3 _sphereExpandPoint;
+    private float _radius;
+
+    // Start is called before the first frame update
+    void Awake() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch) > 0.5f) {
+            //Mache kugel sichtbar
+             //if analogstick
+                //verändere Radius von Kugel
+
+
+            //else
+                // mache Kugel unsichtbar
+        }
+    }
+}
