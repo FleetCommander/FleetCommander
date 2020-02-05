29db19636997bc160d28f00945b6e6a852882c9a
degu1995 <dennis.gulczynski@stud.uni-due.de>
2020-02-05 02:56:50 +0100

    LevelDesign
    Level Design (LvL1 Nav1):
    - Planeten hinzugefügt
    - Minen hinzugefügt
    (Bewegungsscript)
    - 3 Ziele
    - 10 Schiffe pro Farbe

---
+++ Assets/Scripts/Minen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minen : MonoBehaviour {
+    private float speed = 10.0f;
+    [SerializeField] private float time = 0f;
+    [SerializeField] private bool left = true;
+    void Update() {
+        if(time <= 25)
+            Move();
+        else
+        {
+            time = 0;
+            left = !left;
+        }
+    }
+    private void Move() {
+        if (left == true) {
+            transform.Translate(-Vector3.right * speed * Time.deltaTime);
+            time += Time.deltaTime;
+        }
+        else {
+            transform.Translate(-Vector3.left * speed * Time.deltaTime);
+            time += Time.deltaTime;
+        }
+    }
+}
